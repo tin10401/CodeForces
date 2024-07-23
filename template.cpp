393b6240a0188c85fcb8fa70414b7cfc926c4eb8
tin10401 <sololtin2001@gmail.com>
2024-07-23 16:33:52 -0700

    TreeSet CECS PROBLEM

--- template.cpp
+++ template.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define allr(x) (x).rbegin(), (x).rend()
 #define ll long long
-using namespace std;
 const static int mod = 1e9 + 7;
 const static string no = "NO\n";
 const static string yes = "YES\n";
-vector<vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+const vector<vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
 void solve()
 {
 }
 
-
 int main()
 {
-
 }
